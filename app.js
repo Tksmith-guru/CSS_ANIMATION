b810b48bc7fa41e215e356aafb0b5d38e6071e4c
Tksmith-guru <crunchyk837@gmail.com>
2025-01-06 12:47:42 +0100

    testing

---
+++ app.js
@@ -0,0 +1,24 @@
+// // // Contact form submission
+// document.getElementById('contact-form').addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     alert('Thank you for your message! We will get back to you soon.');
+//   });
+
+const contactForm = document.getElementById('contact-form');
+
+contactForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  const subject = `New Message From ${name}`;
+  
+  const whatsappMesage = `Name: ${name} \n Email: ${email} \n ${message}`;
+  const recipientNumber = `+2349035569124`;
+  const whatappUrl = `https://wa.me/${+2349035569124}?text=${encodeURIComponent(whatsappMesage)}`;
+  window.location.href = whatappUrl;
+  alert(`Your message has been sent via WhatsApp`);
+});
+
